4acaaf3115d7710be130bc683761f7b025dd5454
wang994huan <775093289@qq.com>
2019-06-05 21:26:10 +0800

    2019-06-05准备根据陆老师的项目开始路由配置以及文件结构的完善

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const indexTpl = require('./views/index.html')
-const { list} = require('./controllers/list')
+const { list } = require('./controllers/list')
 
-const renderedIndexTpl = template.render(indexTpl,{})
+const renderedIndexTpl = template.render(indexTpl, {})
 
 $('#app').html(renderedIndexTpl)
+
+let swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next'
+    },
+    setWrapperSize: true,
+})
